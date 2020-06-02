726772279d3bc8450b6de710629a5e2b42043f0d
“teji1998” <tejasvinirpk@gmail.com>
2020-06-02 13:05:06 +0530

    Refactored:by adding function to compute wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 echo "Welcome to employee wage computation "
 partTime=1;
@@ -9,16 +8,24 @@ workingDays=20;
 totalSalary=0;
 totalEmpHrs=0;
 totalWorkingDays=0;
+
+function  getWorkingHours(){
+case $1 in
+        $fullTime) empHrs=8;;
+        $partTime) empHrs=4;;
+                   *) empHrs=0;;
+esac
+echo $empHrs
+}
 while [[ $totalEmpHrs -lt $maxHrs && $totalWorkingDays -lt $workingDays ]]
 do
         ((totalWorkingDays++))
-        empCheck=$(($RANDOM%3))
-        case $empCheck in
-        	$fullTime) empHrs=8;;
-        	$partTime) empHrs=4;;
-                   	*) empHrs=0;;
-esac
-totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        #empCheck=$(($RANDOM%3))
+        WorkingHrs=$(getWorkingHours  $(($RANDOM%3)) )
+        totalEmpHrs=$(($totalEmpHrs+$WorkingHrs))
 done
 totalSalary=$(($totalEmpHrs*$ratePerHr))
 echo $totalSalary
+
+
+       
