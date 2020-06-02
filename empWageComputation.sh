4ffeb9bf11bcb9fea2a65c4fefe622927cc6a43a
“teji1998” <tejasvinirpk@gmail.com>
2020-06-02 12:44:35 +0530

    Modified:Display day with wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 echo "Welcome to employee wage computation "
-partTime=1
-fullTime=2
+partTime=1;
+fullTime=2;
 ratePerHr=20;
+maxHrs=100
 workingDays=20;
 totalSalary=0;
-for (( i=1; i<=$workingDays; i++ ))
-do
-        empCheck=$(($RANDOM%3))
-        case $empCheck in
-        	$fullTime) empHrs=8;;
-        	$partTime) empHrs=4;;
-                   	*) empHrs=0;;
+totalEmpHrs=0;
+totalWorkingDays=0;
+declare -A dailyWage
+function  getWorkingHours(){
+case $1 in
+        $fullTime) empHrs=8;;
+        $partTime) empHrs=4;;
+                   *) empHrs=0;;
 esac
-salary=$((empHrs*ratePerHr))
-totalSalary=$(($salary+$totalSalary))
+echo $empHrs
+}
+echo -e "Day No.\t\tDailyWage\tTotalWage" 
+
+while [[ $totalEmpHrs -lt $maxHrs && $totalWorkingDays -lt $workingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$(($RANDOM%3))
+        WorkingHrs=$(getWorkingHours  $empCheck )
+        totalEmpHrs=$(($totalEmpHrs+$WorkingHrs))
+	dailyWage["$totalWorkingDays"]=$(($WorkingHrs*$ratePerHr))
+	totalSalary=$(($totalSalary+${dailyWage["$totalWorkingDays"]}))
+	echo -e $totalWorkingDays "\t\t"${dailyWage["$totalWorkingDays"]}"\t\t"$totalSalary
 done
-echo $totalSalary
