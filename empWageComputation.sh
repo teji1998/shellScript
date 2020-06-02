475e61341126f975a82dd539b41573ab0df4f98a
“teji1998” <tejasvinirpk@gmail.com>
2020-06-02 12:16:34 +0530

    Added:Part time employee case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "Welcome to employee wage computation "
-if [[ $(($RANDOM%2)) -eq 1 ]]
+PartTime=1
+FullTime=2
+ratePerHr=20;
+if [[ $(($RANDOM%3)) -eq 1 ]]
 then
-         ratePerHr=20;
-         empHrs=8;
-         salary=$(($ratePerHr*$empHrs))
+        empHrs=4;
+elif [[ $(($RANDOM%3)) -eq 2 ]]
+then
+        empHrs=8;
 else
-        salary=0;
+        empHrs=0;
 fi
+salary=$(($ratePerHr*$empHrs))
 echo $salary
-
-
