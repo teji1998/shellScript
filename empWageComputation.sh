6ce3f43270173eddac19d50101c7001f02e24f1f
“teji1998” <tejasvinirpk@gmail.com>
2020-06-02 12:53:19 +0530

    Added:Total WOrking hour or day

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 echo "Welcome to employee wage computation "
 partTime=1;
@@ -8,24 +9,16 @@ workingDays=20;
 totalSalary=0;
 totalEmpHrs=0;
 totalWorkingDays=0;
-declare -A dailyWage
-function  getWorkingHours(){
-case $1 in
-        $fullTime) empHrs=8;;
-        $partTime) empHrs=4;;
-                   *) empHrs=0;;
-esac
-echo $empHrs
-}
-echo -e "Day No.\t\tDailyWage\tTotalWage" 
-
 while [[ $totalEmpHrs -lt $maxHrs && $totalWorkingDays -lt $workingDays ]]
 do
         ((totalWorkingDays++))
         empCheck=$(($RANDOM%3))
-        WorkingHrs=$(getWorkingHours  $empCheck )
-        totalEmpHrs=$(($totalEmpHrs+$WorkingHrs))
-	dailyWage["$totalWorkingDays"]=$(($WorkingHrs*$ratePerHr))
-	totalSalary=$(($totalSalary+${dailyWage["$totalWorkingDays"]}))
-	echo -e $totalWorkingDays "\t\t"${dailyWage["$totalWorkingDays"]}"\t\t"$totalSalary
+        case $empCheck in
+        	$fullTime) empHrs=8;;
+        	$partTime) empHrs=4;;
+                   	*) empHrs=0;;
+esac
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
+totalSalary=$(($totalEmpHrs*$ratePerHr))
+echo $totalSalary
